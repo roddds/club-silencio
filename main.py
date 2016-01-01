3d0d1e3376dd2b44f64bf58ad2336f8791737927
Rodrigo Deodoro <roddds@gmail.com>
2016-01-01 13:59:58 -0200

    views

--- main.py
+++ main.py
@@ -3,10 +3,28 @@ from flask import Flask
 from flask import render_template
 
 app = Flask(__name__)
+app.debug = True
+
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/music')
+def music():
+    return render_template('music.html')
+
+@app.route('/live')
+def live():
+    return render_template('live.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
 if __name__ == '__main__':
     app.run()
