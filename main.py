8851476aa2d7d650c5c8d3a727922475a8af5c1f
Rodrigo Deodoro <roddds@gmail.com>
2016-02-09 13:40:28 -0200

    favicon

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 #! /usr/bin/env python3
-from flask import Flask
+import os
+from flask import Flask, send_from_directory
 from flask import render_template
 
 app = Flask(__name__)
 app.debug = True
 
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 @app.route('/')
 def home():
