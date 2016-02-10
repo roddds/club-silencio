a8531d4392bc87e5cd293fe42f691f8cf774c56c
Rodrigo Deodoro <roddds@gmail.com>
2016-02-09 23:54:00 -0200

    trailing slashes

--- main.py
+++ main.py
@@ -16,19 +16,19 @@ def favicon():
 def home():
     return render_template('home.html')
 
-@app.route('/about')
+@app.route('/about/')
 def about():
     return render_template('about.html')
 
-@app.route('/music')
+@app.route('/music/')
 def music():
     return render_template('music.html')
 
-@app.route('/live')
+@app.route('/live/')
 def live():
     return render_template('live.html')
 
-@app.route('/contact')
+@app.route('/contact/')
 def contact():
     return render_template('contact.html')
 
