37c8ead4d911e6dee08958967e6fa6454fd70a16
Rodrigo Deodoro <roddds@gmail.com>
2016-02-10 04:07:11 -0200

    freeze with static url

--- freeze.py
+++ freeze.py
@@ -2,7 +2,10 @@
 from flask_frozen import Freezer
 from main import app
 
-app.config['FREEZER_BASE_URL'] = 'http://roddds.github.io/club-silencio/'
+BASE_URL = 'http://roddds.github.io/club-silencio/'
+
+app.config['FREEZER_BASE_URL'] = BASE_URL
+app.static_url_path = BASE_URL + 'static'
 
 freezer = Freezer(app)
 
