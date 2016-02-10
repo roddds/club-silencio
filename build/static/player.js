e4d4636874af40f09973289f60a315d1404b2ae0
Rodrigo Deodoro <roddds@gmail.com>
2016-02-10 00:14:02 -0200

    build directory

---
+++ build/static/player.js
@@ -0,0 +1,65 @@
+Amplitude.init({
+    "songs": [
+        {
+            name: "Eulogy",
+            artist: "Club Silencio",
+            album: "Dead Beats (2016)",
+            url: "/static/dead_beats/eulogy.mp3"
+        },
+        {
+            name: "Runaway",
+            artist: "Club Silencio",
+            album: "Dead Beats (2016)",
+            url: "/static/dead_beats/runaway.mp3"
+        },
+        {
+            name: "The Lonely Street",
+            artist: "Club Silencio",
+            album: "Dead Beats (2016)",
+            url: "/static/dead_beats/the_lonely_street.mp3"
+        },
+        {
+            name: "Believe",
+            artist: "Club Silencio",
+            album: "Dead Beats (2016)",
+            url: "/static/dead_beats/believe.mp3"
+        },
+        {
+            name: "Untitled",
+            artist: "Club Silencio",
+            album: "Silencio EP (2008)",
+            url: "/static/silencio_ep/untitled.mp3",
+        },
+        {
+            name: "Wouldn't It Be Nice?",
+            artist: "Club Silencio",
+            album: "Silencio EP (2008)",
+            url: "/static/silencio_ep/wouldnt_it_be_nice.mp3",
+        },
+        {
+            name: "Like In A Silent Film",
+            artist: "Club Silencio",
+            album: "Silencio EP (2008)",
+            url: "/static/silencio_ep/like_in_a_silent_film.mp3",
+        },
+        {
+            name: "Puking Butterflies",
+            artist: "Club Silencio",
+            album: "Silencio EP (2008)",
+            url: "/static/silencio_ep/puking_butterflies.mp3",
+        },
+        {
+            name: "Reminder",
+            artist: "Club Silencio",
+            album: "Silencio EP (2008)",
+            url: "/static/silencio_ep/reminder.mp3",
+        },
+        {
+            name: "Fail, Retry",
+            artist: "Club Silencio",
+            album: "Silencio EP (2008)",
+            url: "/static/silencio_ep/fail_retry.mp3",
+        },
+    ],
+    "default_album_art": "/static/dead_beats.png"
+});
