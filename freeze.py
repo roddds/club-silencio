47909d60618cf2b140633b62f4b5117cad007cb0
Rodrigo Deodoro <roddds@gmail.com>
2016-02-10 00:28:40 -0200

    fixing build relative urls

--- freeze.py
+++ freeze.py
@@ -2,6 +2,8 @@
 from flask_frozen import Freezer
 from main import app
 
+app.config['FREEZER_BASE_URL'] = 'http://roddds.github.io/club-silencio/'
+
 freezer = Freezer(app)
 
 if __name__ == '__main__':
